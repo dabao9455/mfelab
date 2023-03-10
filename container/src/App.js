b359b2f89d1393e7a68b6850154ab10132437917
Kevin Ji <kvnji@amazon.com>
2023-03-09 16:34:13 -0800

    Change title

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { createComponent } from "product/productIndex";
-
 const { addHandler, Component } = createComponent();
 
 export default () => {
   return (
     <>
-      <div>I am a app</div>
+      <div>Micro-Frontend Demo</div>
       <Component onItemDeleted={console.log} />
       <button onClick={() => addHandler(0)}>Add</button>
     </>
